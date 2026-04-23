b9558b3dcc5db1721ca0d03da4d23c9eef1a4de0
Gaza Alfath <gazfath12@gmail.com>
2026-04-23 13:00:23 +0700

    style: change PUSDIKLAT logo text color to white

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -27,7 +27,7 @@ export default function AdminLogin() {
             alt="Pusdiklat ISALAM Logo" 
             width={70} 
             height={70} 
-            className={styles.logoImg}
+            style={{ objectFit: "contain" }}
           />
         </div>
 
